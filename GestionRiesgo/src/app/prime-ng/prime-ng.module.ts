d969039ce876bc30cf0253b7e1523603194f7331
Juanes07 <juanesdm3@gmail.com>
2022-06-25 09:05:25 -0500

    ADD:  Se agrega Modulo Prime-NG

---
+++ GestionRiesgo/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {RatingModule} from 'primeng/rating';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ToastModule} from 'primeng/toast';
+import {MenuModule} from 'primeng/menu';
+import {DividerModule} from 'primeng/divider';
+import {DialogModule} from 'primeng/dialog';
+import {CalendarModule} from 'primeng/calendar';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    RatingModule,
+    ProgressSpinnerModule,
+    MessagesModule,
+    MessageModule,
+    ToastModule,
+    MenuModule,
+    DividerModule,
+    DialogModule,
+    CalendarModule
+  ]
+})
+export class PrimeNgModule { }

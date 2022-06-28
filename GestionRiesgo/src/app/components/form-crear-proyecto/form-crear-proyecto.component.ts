2229b045f8c77482c2a0f0974c2cf1b1c870b429
Juanes07 <juanesdm3@gmail.com>
2022-06-28 16:58:27 -0500

    UPDATE: Formulario Riesgo, Servicio guardar Proyecto, Validacion Lider proyecto
     se completa el formulario para guardar un riesgo a su proyecto correspondiente, se hace consumo para guardar el riesgo en el back. se hace correcion en el formulario de crear proyecto para agregar solo correos en el campo de lider proyecto

--- GestionRiesgo/src/app/components/form-crear-proyecto/form-crear-proyecto.component.ts
+++ GestionRiesgo/src/app/components/form-crear-proyecto/form-crear-proyecto.component.ts
@@ -13,30 +13,29 @@ import { idProyectoModel } from 'src/app/models/idproyecto-modelo.model';
 import { proyecto } from 'src/app/models/proyecto-modelo.model';
 import { ProyectoService } from 'src/app/service/proyecto-servicio.service';
 
-
 @Component({
   selector: 'app-form-crear-proyecto',
   templateUrl: './form-crear-proyecto.component.html',
   styleUrls: ['./form-crear-proyecto.component.css'],
 })
 export class FormCrearProyectoComponent implements OnInit {
-
   algo!: number;
 
-  proyecto:idProyectoModel = {
+  proyecto: idProyectoModel = {
     id: '62b874ba0a86251126ce9444',
     idProyecto: this.algo,
-  }
+  };
 
   idnumero: number = 0;
-  responsable:[] = [];
+  responsable: [] = [];
 
   etiquetshtml: string = '';
 
   responsableHtml: string = '';
+  liderHtml: string = '';
 
   formulario: proyecto = {
-    id: null,
+    id: 0,
     nombre: '',
     fechaInicio: '',
     fechaFin: '',
@@ -47,8 +46,6 @@ export class FormCrearProyectoComponent implements OnInit {
     estado: 'creado',
   };
 
-
-
   constructor(
     private formBuilder: FormBuilder,
     private route: Router,
@@ -56,55 +53,21 @@ export class FormCrearProyectoComponent implements OnInit {
     private messageService: MessageService
   ) {}
 
-
-
-
-  ngOnInit() {
-    this.obtenerSecuenciaIdproyecto()
-
-  }
+  ngOnInit() {}
 
   public form: FormGroup = this.formBuilder.group({
     name: ['', Validators.required],
     email: [Validators.email],
-    fecha: ['',[Validators.required]],
-    detalle:['',[Validators.required, Validators.minLength(5)]]
+    fecha: ['', [Validators.required]],
+    detalle: ['', [Validators.required, Validators.minLength(5)]],
+    emailLider: ['', [Validators.email]],
   });
-
-
-  obtenerSecuenciaIdproyecto(): void{
-    this.services.getIdProyecto("62b874ba0a86251126ce9444")
-    .subscribe((data) => (this.proyecto.idProyecto = data.idProyecto))
-  }
-
-  actualizarSecuenciaId():void{
-    let modelo: idProyectoModel={
-      id:'62b874ba0a86251126ce9444',
-      idProyecto: 0,
-    }
-    this.services.getIdProyecto("62b874ba0a86251126ce9444")
-    .subscribe((data) => {
-      modelo.idProyecto = data.idProyecto + 1
-      this.services.actualizarSecuenciaIdProyecto(modelo)
-    })
-
-
-  }
-
-
 
   guardarProyecto(proyecto: proyecto): void {
     this.cambiarFormatoDate();
-   this.actualizarSecuenciaId();
-    if (this.form.value.name && this,this.form.value.fecha) {
-      this.services.getIdProyecto("62b874ba0a86251126ce9444")
-      .subscribe((data) => {
-        this.proyecto.idProyecto = data.idProyecto
-        this.formulario.id = data.idProyecto
-        this.services.guardarProyecto(this.formulario).subscribe({
-        });
-
-      })
+    if ((this.form.value.name && this, this.form.value.fecha)) {
+      this.services.guardarProyecto(this.formulario).subscribe({});
+      this.liderHtml = '';
       this.messageService.add({
         severity: 'success',
         summary: '!Exitoso¡',
@@ -117,6 +80,7 @@ export class FormCrearProyectoComponent implements OnInit {
         detail: '(campos-vacios) validar campos requeridos',
       });
     }
+
     console.log(this.formulario);
   }
 
@@ -125,10 +89,26 @@ export class FormCrearProyectoComponent implements OnInit {
     console.log(this.formulario.etiquetas);
   }
 
-
   agregarResponsable(responsable: string): void {
-    if ( this.metodoComprobarFormatoCorreo(responsable)) {
+    if (this.metodoComprobarFormatoCorreo(responsable)) {
       this.formulario.responsables.push(responsable);
+      this.messageService.add({
+        severity: 'succes',
+        summary: '!Exitoso¡',
+        detail: 'Responsable guardado ',
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Rectifique los datos',
+        detail: 'Responsable no guardado (validar formato correo) ',
+      });
+    }
+  }
+
+  agregarLider(responsable: string): void {
+    if (this.metodoComprobarFormatoCorreo(responsable)) {
+      this.formulario.liderProyecto = responsable;
       this.messageService.add({
         severity: 'succes',
         summary: '!Exitoso¡',
@@ -144,14 +124,11 @@ export class FormCrearProyectoComponent implements OnInit {
 
   }
 
-
-  metodoComprobarFormatoCorreo(responsable: string){
-    let result  = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+  metodoComprobarFormatoCorreo(responsable: string) {
+    let result = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
     let otra = result.test(responsable);
-    return otra
+    return otra;
   }
-
-
 
   cambiarFormatoDate() {
     const date = this.formulario.fechaInicio;
@@ -160,8 +137,8 @@ export class FormCrearProyectoComponent implements OnInit {
     this.formulario.fechaInicio = myDate;
     const date2 = this.formulario.fechaFin;
     const myDate2 = moment(date2, 'YYYYMMDDTHHmmss').format(myFormat);
-    if(myDate2 === 'Invalid date'){
-      this.formulario.fechaFin = ''
+    if (myDate2 === 'Invalid date') {
+      this.formulario.fechaFin = '';
     } else {
       this.formulario.fechaFin = myDate2;
     }

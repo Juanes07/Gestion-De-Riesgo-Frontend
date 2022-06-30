927cde0838f66113d53800c4e5a9cd46d5036d77
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-06-29 20:28:03 -0500

    ADD: MatrizRiesgos sin filtros
    Se agrega tabla para mostrar matriz de calor de riesgos por proyecto

--- GestionRiesgo/src/app/components/matriz-riesgo/matriz-riesgo.component.ts
+++ GestionRiesgo/src/app/components/matriz-riesgo/matriz-riesgo.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { riesgo } from 'src/app/models/riesgo-modelo.models';
+import { ProyectoService } from 'src/app/service/proyecto-servicio.service';
 
 @Component({
   selector: 'app-matriz-riesgo',
   templateUrl: './matriz-riesgo.component.html',
-  styleUrls: ['./matriz-riesgo.component.css']
+  styleUrls: ['./matriz-riesgo.component.css'],
 })
 export class MatrizRiesgoComponent implements OnInit {
+  riesgo!: riesgo;
 
-  constructor() { }
+  matrizBi: Array<Array<number>> = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+  ];
+
+  constructor(
+    private service: ProyectoService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.metodo();
   }
 
+  metodo() {
+    this.route.parent?.params.subscribe((params) => {
+      this.service.getRiesgosByProyectoId(params['id']).subscribe((data) => {
+        for (let riesgo = 0; riesgo < data.length; riesgo++) {
+          this.riesgo = data[riesgo];
+          for (let fila = 0; fila < this.matrizBi.length; fila++) {
+            for (let columna = 0; columna < this.matrizBi.length; columna++) {
+              if (
+                this.riesgo.probabilidadDeOcurrenciaDelRiesgo -1 == fila &&
+                this.riesgo.impactoDeOcurrenciaDelRiesgo -1 == columna
+              ) {
+                this.matrizBi[fila][columna] += 1;
+              }
+            }
+          }
+        }
+        console.log(this.matrizBi);
+      });
+    });
+  }
 }
